56e5d0128ad8b4bd95516793c980508ea4e3fc36
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-07-05 17:10:01 -0400

    Example is working

---
+++ example/scripts/build-example.ts
@@ -0,0 +1,45 @@
+import * as esbuild from 'npm:esbuild'
+
+await esbuild.build({
+  entryPoints: ['src/index.tsx'],
+  bundle: true,
+  outdir: 'dist',
+  platform: 'browser',
+  conditions: ['edge-light', 'edge'],
+  format: 'esm',
+  jsx: 'automatic',
+  minify: true,
+})
+
+let output = `./.vercel/output`
+
+let funcs = `${output}/functions`
+
+try {
+  Deno.removeSync('./.vercel', { recursive: true })
+} catch (e) {
+  // ignored
+}
+
+Deno.mkdirSync(`${funcs}/index.func`, { recursive: true })
+Deno.copyFileSync('./dist/index.js', `${funcs}/index.func/index.js`)
+
+let vcConfig = {
+  runtime: 'edge',
+  entrypoint: 'index.js',
+}
+
+let textEncoder = new TextEncoder()
+let vcConfigEncoded = textEncoder.encode(JSON.stringify(vcConfig, null, 2))
+
+Deno.writeFileSync(`${funcs}/index.func/.vc-config.json`, vcConfigEncoded)
+
+let generalConfig = {
+  version: 3,
+}
+
+let generalConfigEncoded = textEncoder.encode(
+  JSON.stringify(generalConfig, null, 2),
+)
+
+Deno.writeFileSync(`${output}/config.json`, generalConfigEncoded)
---
+++ example/src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+  getReactRequestHandler,
+  FallbackProps,
+  AppProps,
+} from '@matthamlin/srvr'
+
+function App({ url }: AppProps) {
+  return (
+    <div>
+      {/* @ts-expect-error */}
+      <marquee>App being served from: {url}</marquee>
+    </div>
+  )
+}
+
+function Fallback({ error }: FallbackProps) {
+  return <pre>{JSON.stringify(error, null, 2)}</pre>
+}
+
+export default function handler(request: Request): Promise<Response> {
+  return getReactRequestHandler({
+    App,
+    Fallback,
+  })(request)
+}
